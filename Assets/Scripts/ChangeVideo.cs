bec5befa58c732669182c30d226f08078cbd115f
JoInHo <zjxps2007@gmail.com>
2024-03-13 14:28:25 +0900

    Update ChangeVideo.cs

--- Assets/Scripts/ChangeVideo.cs
+++ Assets/Scripts/ChangeVideo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,15 +10,33 @@ public class ChangeVideo : MonoBehaviour
     public VideoClip clip02;
     private bool isclip01;
     private bool isclip02;
-    
-    
+
+    private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        videoPlayer = gameObject.GetComponent<VideoPlayer>();
+    }
+
+
     public void Change01()
     {
         if (!isclip01)
         {
-            gameObject.GetComponent<VideoPlayer>().clip = clip01;
+            videoPlayer.clip = clip01;
             isclip01 = true;
             isclip02 = false;
+        }
+        else
+        {
+            if (videoPlayer.isPlaying)
+            {
+                videoPlayer.Pause();
+            }
+            else
+            {
+                videoPlayer.Play();
+            }
         }
     }
 
@@ -25,9 +44,20 @@ public class ChangeVideo : MonoBehaviour
     {
         if (!isclip02)
         {
-            gameObject.GetComponent<VideoPlayer>().clip = clip02;
+            videoPlayer.clip = clip02;
             isclip02 = true;
             isclip01 = false;
+        }
+        else
+        {
+            if (videoPlayer.isPlaying)
+            {
+                gameObject.GetComponent<VideoPlayer>().Pause();
+            }
+            else
+            {
+                gameObject.GetComponent<VideoPlayer>().Play();
+            }
         }
     }
 }
